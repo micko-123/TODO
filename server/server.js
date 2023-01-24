31ec5bf24720e180c66fd40cf2747d4dda1d3bdb
Mickyas Asnake <60357226+micko-123@users.noreply.github.com>
2023-01-23 17:49:40 -0800

    update todo added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+
 const todoRouter = require('./routers/todo.router')
 const connectDB = require('./config/db')
-
 const app = express()
 
 
 // connect to database
 connectDB() 
 
+app.use(cors())
 app.use(express.json())
 
 
